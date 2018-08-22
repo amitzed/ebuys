73b35e70792d486bc4ef400fd6fdccfaf5efd638
Amit <amit@AZ-iMac.local>
2018-08-22 16:55:49 -0400

    added CSS paralex effect on main image

--- public/js/app.js
+++ public/js/app.js
@@ -3,10 +3,18 @@ class App extends React.Component {
     return (
       <div className='section'>
         <div className='header'>
-          <img src="images/eb-logo.jpg" alt="EB" />
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          <br/><br/><br/><br/><br/><br/><br/><br/>
+          <p><img src="images/eb-logo.jpg" alt="EB" /></p>
         </div>
+        <br/>
         <h1 className='title'> eBuys <br/><h6>A Wish List for the Armchair Shopaholic</h6></h1>
         <br/>
+
+        <div className="progress-container">
+          <div className="progress-bar" id="myBar"></div>
+        </div>
+
         <div className='columns'>
         <Items />
 
