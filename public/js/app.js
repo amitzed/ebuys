195ff12cbe078314b1a50befd5d5300d77c8f11a
Amit <amit@AZ-iMac.local>
2018-08-22 20:31:37 -0400

    Added some CSS animations

--- public/js/app.js
+++ public/js/app.js
@@ -6,15 +6,12 @@ class App extends React.Component {
           <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
           <br/><br/><br/><br/><br/><br/><br/><br/>
           <p><img src="images/eb-logo.jpg" alt="EB" /></p>
+          <br/>
+          <h1 className='title'> eBuys </h1>
+          <h6>A Wish List for the Armchair Shopaholic</h6>
         </div>
         <br/>
-        <h1 className='title'> eBuys <br/><h6>A Wish List for the Armchair Shopaholic</h6></h1>
         <br/>
-
-        <div className="progress-container">
-          <div className="progress-bar" id="myBar"></div>
-        </div>
-
         <div className='columns'>
         <Items />
 
